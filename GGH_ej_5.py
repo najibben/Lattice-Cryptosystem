c68b619805f54808025c53e9dc1b8ae969ebccae
najibben <ben.haddou.perez.najib@gmail.com>
2022-05-08 18:41:04 -0400

    first commit

---
+++ GGH_ej_5.py
@@ -0,0 +1,94 @@
+''' This program is an implementation of the GGH cryptosystem. 
+The user inputs the name of the file containing private key on the command line. For example
+"python ggh.py input.txt". The input file must contain a square matrix with base vectors of the private key as rows.
+Coordinates must be separated by a space. Then the program prompts the user for the integer vector they wish to encrypt.
+Then the program outputs the encrypted message and decrypted message, providing that the used private key is orthogonal enough.
+NOTE: GGH should NOT be used to encrypt anything as it has been broken. 
+This program is just an example of how the cryptosystem works.'''
+
+import numpy as np
+import sys
+
+ERROR_VECTOR = np.array([-1, 1, 1])
+
+
+def input_handler(filename):
+	try:
+		with open(filename, 'r') as infile:
+			n = sum((1 for line in infile)) #counts lines in infile. This is the expected dimension of the matrix.
+			matrix = np.empty([n, n])
+			i = 0
+			infile.seek(0)
+			for line in infile:
+				matrix[i] = [int(x) for x in line.split()]
+				i = i + 1
+	except Exception as e:
+		print(e)
+		sys.exit(0)
+	return matrix, n
+
+	'''generate_public_key takes as input the private key as a matrix and its dimension. 
+	Then it multiplies the private key with 5 random unimodular matrices from the right hand side thus creating the public key and outputs it'''
+def generate_public_key(n, pri_key):
+	k = 0
+	matrix = np.identity(n)
+	while k < 5:
+		A = np.random.randint(-5, 5, size=(n, n))
+		if np.linalg.det(A) == 1:
+			matrix = np.matmul(matrix, A)
+			k = k + 1
+	pub_key = np.matmul(matrix, pri_key)
+	return pub_key;
+	
+	'''encryption takes as input the message, the public key and the error vector and outputs the encrypted message.'''
+def encryption(m, pub_key, e):
+	c = np.matmul(m, pub_key) + e
+	return c;
+	
+	'''decryption uses Babai's algorithm to decrypt the encrypted message and outputs the original message (if the private key is orthogonal enough).'''
+def decryption(c, pri_key, pub_key):
+	u = np.matmul(c, np.linalg.inv(pri_key))
+	u = np.matmul(np.rint(u), pri_key)
+	result = np.matmul(u, np.linalg.inv(pub_key))
+	return result;
+
+
+def check_determinant(matrix):
+	det=1
+
+	for i in range(len(matrix)):
+		for j in range(len(matrix)):
+			if i ==j:
+				det = det * matrix[i][j]
+	return det
+
+
+
+def perturbation(m, pub_key, c):
+	e = c - np.matmul(m, pub_key)
+	return e;
+
+
+#filename = sys.argv[-1]
+#private_key, dimension = input_handler(filename)
+#public_key = generate_public_key(dimension, private_key)
+public_key  = np.array([[324850,-1625176,2734951],[165782,-829409,1395775],[485054,-2426708,4083804]])
+#message = np.empty(dimension)
+#message = [int(x) for x in input("Input the integer vector you wish to encrypt ").split()]
+#message = np.array([[3,-4,1,3]])
+#encrypted = encryption(message, public_key, ERROR_VECTOR)
+encrypted = np.array([[8930810,-44681748, 75192665]])
+#print(encrypted)
+private_key = np.array([[58,53,68],[110,112,35],[10,119,123]])
+decrypted = decryption(encrypted, private_key, public_key)
+print(decrypted)
+#m = decryption(encrypted, private_key, public_key)
+m =[[714.94261706,3676.99362826,-1717.12356341]]
+r = perturbation(m,public_key,encrypted)
+print (r)
+det = check_determinant(public_key)
+print(det)
+
+#m =[[  714.94261706  3676.99362826 -1717.12356341]]
+# r = [[ -3.90586627 -45.47095394 -65.20745468]]
+# d = -1100313660777924600
